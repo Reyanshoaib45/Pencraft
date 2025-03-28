2bc213ed37a58a688ae3a5943c15ab158227a6dd
M Reyan Shoaib <reyanshoaib45@gmail.com>
2025-03-28 14:19:45 +0500

    working seo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('seo', function ($app) {
+            return new SeoService();
+        });
     }
 
     /**
