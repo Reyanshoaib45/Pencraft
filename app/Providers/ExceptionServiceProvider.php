dc3a7260afe99c51ca2ca2132b22f25c8e588934
M Reyan Shoaib <reyanshoaib45@gmail.com>
2025-04-14 21:02:14 +0500

    add provider and update

--- app/Providers/ExceptionServiceProvider.php
+++ app/Providers/ExceptionServiceProvider.php
@@ -3,14 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class ExceptionServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // Force Laravel to use classic Blade error views
-        $this->app->singleton('Illuminate\Contracts\Debug\ExceptionHandler', function ($app) {
-            return new \Illuminate\Foundation\Exceptions\Handler($app['config']['app.debug']);
+        // Fix: Pass the correct container instance to the handler constructor
+        $this->app->singleton(ExceptionHandler::class, function (Container $app) {
+            return new ExceptionHandler($app);
         });
     }
 }
