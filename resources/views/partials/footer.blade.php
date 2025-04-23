37368cd20710c80488f4491398bbe5f2e48d6afe
M Reyan Shoaib <reyanshoaib45@gmail.com>
2025-04-23 09:41:27 +0500

    working on loqout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -166,14 +166,19 @@
                         </svg>
                     </a>
                 @else
-                    <a href="{{ route('logout') }}" class="text-black hover:text-gray-600 flex items-center">
-                        Sign out
-                        <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M5 12h14"></path>
-                            <path d="m12 5 7 7-7 7"></path>
-                        </svg>
-                    </a>
+                    <form action="{{ route('logout') }}" method="POST" class="inline">
+                        @csrf
+                        <button type="submit" class="text-black hover:text-gray-600 flex items-center">
+                            Sign out
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round">
+                                <path d="M5 12h14"></path>
+                                <path d="m12 5 7 7-7 7"></path>
+                            </svg>
+                        </button>
+                    </form>
+
                 @endguest
             </div>
         </div>
